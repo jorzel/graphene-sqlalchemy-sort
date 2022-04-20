6c720bce48a5d570189a420b769da8ee6451dc1f
Jarosław Orzeł <jaroslaw.orzel@emplocity.pl>
2022-04-20 15:50:26 +0200

    Custom sorting

--- src/tests/test_schema.py
+++ src/tests/test_schema.py
@@ -79,3 +79,34 @@ def test_examples_sorting_default_field_DESC(example_factory, db_session):
 
     assert result.errors is None
     assert result.data == expected_result
+
+
+def test_examples_sorting_custom_field_DESC(example_factory, db_session):
+    example1 = example_factory(first_name="test", second_name="bmarek")
+    example2 = example_factory(first_name="jurek", second_name="ak")
+    example3 = example_factory(first_name="atest", second_name=None)
+    query = """
+        {
+            examples (sort: {name: "ASC"}) {
+                edges {
+                    node {
+                        firstName
+                    }
+                }
+            }
+        }
+    """
+    expected_result = {
+        "examples": {
+            "edges": [
+                {"node": {"firstName": example2.first_name}},
+                {"node": {"firstName": example3.first_name}},
+                {"node": {"firstName": example1.first_name}},
+            ]
+        }
+    }
+
+    result = schema.execute(query, context_value={"session": db_session})
+
+    assert result.errors is None
+    assert result.data == expected_result
